067bf4558674433485f91ee2224c2a7e2d6c8b5d
Anmol <anmolkrjha006@gmail.com>
2025-06-26 13:12:33 +0530

    Creates the movie booking page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -16,7 +16,7 @@ const MovieCard = ({ movie }) => {
 
       <div className='flex items-center justify-between mt-4 pb-3'>
         <button onClick={() => { navigate(`/movies/${movie._id}`); scrollTo(0, 0) }} className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>
-          Buy Tickets
+          Buy Ticket
         </button>
         <p className='flex items-center gap-1 text-s text-gray-400 mt-1 pr-1'>
           <StarIcon className='w-4 h-4 text-primary fill-primary' />
--- src/components/TrailerSection.jsx
+++ src/components/TrailerSection.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { dummyTrailers } from '../assets/assets';
 import ReactPlayer from 'react-player';
 import BlurCircle from './BlurCircle';
+import { PlayCircleIcon } from 'lucide-react';
 
 const TrailerSection = () => {
     const [currentTrailer, setcurrentTrailer] = useState(dummyTrailers[0]);
@@ -12,12 +13,13 @@ const TrailerSection = () => {
                 <BlurCircle top='-100px' right='-100px' />
                 <ReactPlayer url={currentTrailer.videoUrl} controls={false} className='mx-auto max-w-full' width="960px" height="540px" />
             </div>
-            <div className='flex flex-row items-center gap-3 w-300 px-10 mt-3'>
-                <img src={dummyTrailers[1].image} className='w-[20%] hover:cursor-pointer' onClick={()=>{setcurrentTrailer(dummyTrailers[1])}}/>
-                <img src={dummyTrailers[2].image} className='w-[20%] hover:cursor-pointer' onClick={()=>{setcurrentTrailer(dummyTrailers[2])}}/>
-                <img src={dummyTrailers[3].image} className='w-[20%] hover:cursor-pointer' onClick={()=>{setcurrentTrailer(dummyTrailers[3])}}/>
-                <img src={dummyTrailers[3].image} className='w-[20%] hover:cursor-pointer' onClick={()=>{setcurrentTrailer(dummyTrailers[3])}}/>
-
+            <div className='group grid grid-cols-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
+                {dummyTrailers.map((trailer) => (
+                    <div key={trailer.image} onClick={() => { setcurrentTrailer(trailer) }} className='relative group-hover:not-hover:opacity-50 hover:-translate-y-1 duration-300 transition max-md:h-60 md:max-h-60 cursor-pointer'>
+                        <img src={trailer.image} alt="trailer" className='rounded-lg w-full h-full object-cover brightness-75' />
+                        <PlayCircleIcon strokeWidth={1.6} className='absolute top-1/2 left-1/2 w-5 md:h-12 transform -translate-x-1/2 -translate-y-1/2' />
+                    </div>
+                ))}
             </div>
         </div>
 
